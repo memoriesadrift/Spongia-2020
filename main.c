6387144ea47cff9b4bc0f0891d91145f7e643300
Samuel Sulovsky <samuel.sulovsky@gmail.com>
2020-10-24 19:33:01 +0200

    added n by n sprite movement/scrolling functions

--- main.c
+++ main.c
@@ -8,6 +8,20 @@ BYTE airborne;
 INT8 gravity; // FIXME: const?
 INT16 currentSpeedY;
 BYTE gameRunning;
+const UINT8 tileSize = 8; // FIXME: doesn't work as #define TILESIZE 8 in move_sprite() function call - why?
+
+// Bigger sprite supporting struct
+struct GameObject 
+{
+    UBYTE spriteids[4];
+    UINT8 x;
+    UINT8 y;
+    UINT8 width;
+    UINT8 height;
+};
+
+// Player vars
+struct GameObject player;
 
 // CPU Efficient waiting function to be used
 // when waiting is needed
@@ -17,6 +31,103 @@ void efficient_wait(INT16 loops)
     for (i; i < loops; ++i)
     {
         wait_vbl_done();
+    }
+}
+
+void setup_player()
+{
+    player.height = 16;
+    player.width = 8;
+    player.x = 80; // placeholder
+    player.y = 72; // placeholder
+    player.spriteids[0] = 0;
+    player.spriteids[1] = 1;
+    set_sprite_tile(0,0);
+    set_sprite_tile(1,1);
+}
+
+// Function to move n x n tile game objects
+void move_game_object(struct GameObject* obj, UINT8 x, UINT8 y)
+{
+    // top left / only sprite
+    move_sprite(obj->spriteids[0], x, y);
+    // FIXME: add constant size offset for sprites
+    
+    //bottom left sprite / top sprite of 16x8 sprites
+    if (obj->height > 8)
+    {
+        move_sprite(obj->spriteids[1], x, y + tileSize);
+    }
+
+    // top right sprite
+    if (obj->width > 8)
+    {
+        move_sprite(obj->spriteids[2], x + tileSize, y);
+    }
+
+    // bottom right sprite
+    if (obj->width > 8 && obj->height > 8)
+    {
+        move_sprite(obj->spriteids[3], x + tileSize, y + tileSize);
+    }
+}
+
+// function to scroll n x n tile game objects
+void scroll_game_object(struct GameObject* obj, INT8 movex, INT8 movey)
+{
+    while(movex != 0 && movey != 0)
+    {
+        if(movex != 0){
+            // top left / only sprite
+            scroll_sprite(obj->spriteids[0], movex < 0 ? -1 : 1, 0);
+            
+            //bottom left sprite / top sprite of 16x8 sprites
+            if (obj->height > 8)
+            {
+                scroll_sprite(obj->spriteids[1], movex < 0 ? -1 : 1, 0);
+            }
+
+            // top right sprite
+            if (obj->width > 8)
+            {
+                scroll_sprite(obj->spriteids[2], movex < 0 ? -1 : 1, 0);
+            }
+
+            // bottom right sprite
+            if (obj->width > 8 && obj->height > 8)
+            {
+                scroll_sprite(obj->spriteids[3], movex < 0 ? -1 : 1, 0);
+            }
+
+            movex += (movex < 0 ? 1 : -1);
+            wait_vbl_done();
+        }
+
+        if(movey != 0){
+                    // top left / only sprite
+            scroll_sprite(obj->spriteids[0], 0, movex < 0 ? -1 : 1);
+            
+            //bottom left sprite / top sprite of 16x8 sprites
+            if (obj->height > 8)
+            {
+                scroll_sprite(obj->spriteids[1], 0, movex < 0 ? -1 : 1);
+            }
+
+            // top right sprite
+            if (obj->width > 8)
+            {
+                scroll_sprite(obj->spriteids[2], 0, movex < 0 ? -1 : 1);
+            }
+
+            // bottom right sprite
+            if (obj->width > 8 && obj->height > 8)
+            {
+                scroll_sprite(obj->spriteids[3], 0, movex < 0 ? -1 : 1;
+            }
+
+            movey += (movey < 0 ? 1 : -1);
+            wait_vbl_done();
+        }
     }
 }
 
