9bb710156f6ac1a5d1fac40ed35cba9747684c33
Samuel Sulovsky <samuel.sulovsky@gmail.com>
2020-10-24 18:36:00 +0200

    engine skeleton implementation

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 
 // TODO: Split generic functions into separate header files eventually?
 
+// Engine related variables
+BYTE airborne;
+INT8 gravity; // FIXME: const?
+INT16 currentSpeedY;
+BYTE gameRunning;
+
 // CPU Efficient waiting function to be used
 // when waiting is needed
 void efficient_wait(INT16 loops)
@@ -17,9 +23,46 @@ void efficient_wait(INT16 loops)
 // Function to setup game
 // to be called at the beginning of main
 // sets up bg, turns on display, etc.
-void setup_game();
+void setup_game()
+{
+    gameRunning = 1;
+}
+
+// Player movement function
+void move_player();
+
+// Jump function
+void jump();
 
 int main()
 {
+    setup_game();
+
+    while(gameRunning)
+    {
+        // this check allows us to use a switch for the movement (prettier) but also enables
+        // moving while in the air, which is nice.
+        if (airborne)
+        {
+            // fall
+        }
+        
+        switch (joypad())
+        {
+        case J_A:
+            if (!airborne)
+                jump();
+            break;
+        case J_LEFT:
+            move_player(/*params*/);
+            break;
+        case J_RIGHT:
+            move_player(/*params*/);
+            break;
+        default:
+            break;
+        }
+    }
+
     return 0;
 }
